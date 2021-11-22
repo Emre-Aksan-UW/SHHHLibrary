f284a8de0554e355322ed86633cb491cda68c3db
Davionli <davionli1007@gmail.com>
2021-11-22 13:55:30 -0800

    item, factory, command

--- SHHHLibrary/factory.h
+++ SHHHLibrary/factory.h
@@ -1,5 +1,22 @@
 #pragma once
-class factory
-{
+#include "item.h"
+#include "command.h"
+#include "client.h"
+#include <string.h>
+
+class Factory {
+private:
+    Item* itemFactory[100];
+    Client* clientFactory[100];
+    Command* commandFactory[4];
+    int itemHash (string str) const;
+    int commandHash(char ch) const;
+    int clientHash(int id) const;
+public:
+    Factory();
+    ~Factory();
+    Item* createItem(string str);
+    Client* createClient(int id);
+    Command* createCommand(char ch);
 };
 
