339fb9983ef60548507116e6b7f97c3af1a6eae4
vsimakova <67300899+vsimakova@users.noreply.github.com>
2021-11-22 13:30:13 -0800

    update

--- SHHHLibrary/childrenbook.h
+++ SHHHLibrary/childrenbook.h
@@ -1,15 +1,18 @@
 #pragma once
-#include <string>
-#include "book.h"
+#include "Book.h"
+
 using namespace std;
 
-class ChildrenBook : public Book
-{
+class ChildrenBook : public Book {
 public:
   ChildrenBook();
+  ChildrenBook(const ChildrenBook&);
   ~ChildrenBook();
-
+  virtual void setData(istream&);
+  virtual void display() const;
+  virtual void displayTitle() const;
+  virtual bool operator==(const Item&) const;
+  
 private:
   string author;
 };
-
