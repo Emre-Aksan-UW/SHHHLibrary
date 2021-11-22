30d9671853bdd22513ba2d612d1c88e1a2bea0e5
Emre-Aksan-UW <81589499+Emre-Aksan-UW@users.noreply.github.com>
2021-11-22 13:29:56 -0800

    Update command.h

--- SHHHLibrary/command.h
+++ SHHHLibrary/command.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "library.h"
 class command
 {
+  public:
+    Command();
+    Command(const Command&);
+    ~Command();
+  
+    void execute();
+    void setData();
+  private:
+    Library library;
+    
 };
 
