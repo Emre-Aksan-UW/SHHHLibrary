d5fd078008eb4fd37b9e38ef74a1ad85f80161dc
Emre-Aksan-UW <81589499+Emre-Aksan-UW@users.noreply.github.com>
2021-11-22 13:21:59 -0800

    Update checkout.h

--- SHHHLibrary/checkout.h
+++ SHHHLibrary/checkout.h
@@ -1,5 +1,16 @@
 #pragma once
+#include "item.h"
+using namespace std;
+
 class checkout
 {
+  public;
+    CheckOut(Library library, int id, Item item);
+    ~CheckOut();
+    int id;
+    Item item;
+  
+  private:
+    void execute(int, Item)
 };
 
