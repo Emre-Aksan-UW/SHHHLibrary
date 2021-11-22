1f54e55608621abee308fbe9d5b91e75b99e14c1
vsimakova <67300899+vsimakova@users.noreply.github.com>
2021-11-22 11:44:05 -0800

    update

--- SHHHLibrary/item.h
+++ SHHHLibrary/item.h
@@ -1,6 +1,28 @@
 #pragma once
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
 class Item {
-    
-       
+public:
+    Item();
+    Item (const Item&);
+
+    virtual ~Item();
+    virtual void setData(istream& ) = 0;
+    virtual void display() const = 0;        //display all items
+    virtual void displayTitle() const = 0;
+    virtual Item* getItem() const = 0;
+    void setMaxCopyCount(const int);
+    void incrementCopyCount();
+    void decrementCopyCount();
+    int getCatalog();
+    int getOutOfCatalog();
+    virtual bool operator==(const Item&) const = 0;
+
+private:
+    int maxCopyCount;
+    int currCopyCount;
 };
 
